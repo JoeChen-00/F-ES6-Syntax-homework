81feaacb7c81367f7c3d5ca6f94dd5ff9178e63e
Wenhao Chen <wenhao.chen@thoughtworks.com>
2020-11-10 20:03:37 +0800

    finish

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
+function parseData({data,column}){
+    let template = [];
+    let result = [];
+    column.map(item=>template.push(item.name));
+    data.forEach(
+        data => {
+            let oneResult = {};
+            for(let F = 0;F < template.length; ++F)
+                oneResult[template[F]] = data[F];
+            result.push(oneResult);
+        }
+    );
+    return result;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+function inject(items, sections){
+    let count = 0;
+    sections.forEach(item => {
+        items.splice(item.index + count++,0,item.content)
+    });
+    return items;
+}
 export { inject };
